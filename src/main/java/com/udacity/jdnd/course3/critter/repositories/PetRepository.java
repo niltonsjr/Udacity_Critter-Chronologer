b897daef525dba54e691eb7a8119d0c258c985d1
Nilton da Silva Junior <tonsti@gmail.com>
2021-04-28 20:32:01 +0200

    Add repositories

---
+++ src/main/java/com/udacity/jdnd/course3/critter/repositories/PetRepository.java
@@ -0,0 +1,7 @@
+package com.udacity.jdnd.course3.critter.repositories;
+
+import com.udacity.jdnd.course3.critter.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+}
